48e02f0554b017629473c744c1104d024e85bb9c
mattcole <mattcole@tracefirst.com>
2012-08-08 11:21:15 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirfieldApp::Application.config.secret_token = 'b8f87f8984b14acf05475bddf3d3b7a622fbf040fa1bf2ca828bcdb7eb0eea61db3c69452e2661ff423779b1a2bbc9cab533ae38aed206d1de327cd3d9bae318'
